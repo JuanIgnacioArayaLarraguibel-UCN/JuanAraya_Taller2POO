f83b11ccdcc1319688f2872433493bc818513a0e
Juan Araya Larraguibel <juan.araya02@alumnos.ucn.cl>
2026-04-13 14:46:17 -0400

    creacion de la clase Pokemon

--- src/JuanAraya_Taller2POO/Main.java
+++ src/JuanAraya_Taller2POO/Main.java
@@ -1,4 +1,7 @@
 package JuanAraya_Taller2POO;
+//Integrante: Juan Ignacio Araya Larraguibel
+//RUT: 21.566.260-8
+//Carrera: Ingenieria en Tecnologias de Información
 
 public class Main {
 
---
+++ src/JuanAraya_Taller2POO/Pokemon.java
@@ -0,0 +1,14 @@
+package JuanAraya_Taller2POO;
+
+public class Pokemon {
+	
+	private String nombre;
+	private String habitat;
+	private double porcentajeAparicion;
+	private int hp, ataque, defensa, ataqueEspecial, defensaEspecial, velocidad;
+	
+	public Pokemon(String nombre, String habitat, double porcentajeAparicion, int hp, int ataque, int defensa, int ataqueEspecial, int defensaEspecial, int velocidad, String tipo) {
+		
+	}
+
+}
