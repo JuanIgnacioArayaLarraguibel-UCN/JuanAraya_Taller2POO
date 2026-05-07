7eb08dc0b35f35a09c306a49c76bbe71bc197b52
Juan Araya Larraguibel <juan.araya02@alumnos.ucn.cl>
2026-05-07 17:20:37 -0400

    empezando con el Main
    llamando las clases correspondientes

--- src/JuanAraya_Taller2POO/Main.java
+++ src/JuanAraya_Taller2POO/Main.java
@@ -3,6 +3,18 @@ package JuanAraya_Taller2POO;
 //RUT: 21.566.260-8
 //Carrera: Ingenieria en Tecnologias de Información
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 //esto va a estar Funny es lo q falta y aqui vienen los commits por metodos ya q hice commits por clases para no tirar muchos
+	
+	private static Scanner scanner = new Scanner(System.in);
+	private static List<Pokemon> pokedex;
+    private static List<Gimnasio> gimnasios;
+    private static List<EliteFour> eliteFour;
+    private static Jugador jugador;
+	public static void main(String[] args) {
+		
+	}
 }
