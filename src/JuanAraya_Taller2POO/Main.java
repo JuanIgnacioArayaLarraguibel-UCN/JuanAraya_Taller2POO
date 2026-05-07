7483faa3e3518da4d1e57a8b4a12f79eae8f5a25
Juan Araya Larraguibel <juan.araya02@alumnos.ucn.cl>
2026-05-07 00:04:54 -0400

    commit de comentario en el Main para empezarlo

--- src/JuanAraya_Taller2POO/Main.java
+++ src/JuanAraya_Taller2POO/Main.java
@@ -4,5 +4,5 @@ package JuanAraya_Taller2POO;
 //Carrera: Ingenieria en Tecnologias de Información
 
 public class Main {
-
+//esto va a estar Funny es lo q falta y aqui vienen los commits por metodos ya q hice commits por clases para no tirar muchos
 }
